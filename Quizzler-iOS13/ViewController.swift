dc5eddac8d94017d37e35e05ad66192ef77eaf5e
kavindadilshan <kavindadilshan97@gmail.com>
2022-08-16 23:50:04 +0530

    added dimensional army process

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -1,20 +1,45 @@
-//
-//  ViewController.swift
-//  Quizzler-iOS13
-//
-//  Created by Angela Yu on 12/07/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
+    //
+    //  ViewController.swift
+    //  Quizzler-iOS13
+    //
+    //  Created by Angela Yu on 12/07/2019.
+    //  Copyright © 2019 The App Brewery. All rights reserved.
+    //
 
-import UIKit
+    import UIKit
 
-class ViewController: UIViewController {
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    class ViewController: UIViewController {
+        
+        @IBOutlet weak var questionLabel: UILabel!
+        @IBOutlet weak var progressBar: UIImageView!
+        
+        let quizes = [
+          ["Are you married","True"],
+          ["Are you single?","False"],
+          ["You have boyfriend?","True"]
+        ]
+        
+        var questionNumber = 0
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            // Do any additional setup after loading the view.
+        }
+
+        @IBAction func buttonAction(_ sender: UIButton) {
+           
+            if (quizes[questionNumber][1] == sender.currentTitle) {
+                print("correct")
+            }else{
+                print("incorrect")
+            }
+            
+            //Array.count to use get length
+            if quizes.count<3{
+                questionNumber+=1
+            }
+        }
+        
+        
     }
-
-
-}
 
